5f22c954b4c617fdd7d5c0239dba1e7fa1cc8217
David Sneek <davidjfk@hotmail.com>
2021-06-18 16:53:16 +0200

    update with feedback from teacher Ben Linnebank

--- js/filmzoeker_script.js
+++ js/filmzoeker_script.js
@@ -1,117 +1,109 @@
-console.log(`Opdracht: Filmzoeker. David Sneek: `)
+const buttonSearchRandom = document.querySelector('.flex-container__random-search-button');
+const inputFieldSearchTerm = document.querySelector('.flex-container__inputfield-search-term');
+const inputSearchButton = document.querySelector('.flex-container__search-button');
+const radios = document.querySelectorAll('input[type=radio][name="film-filter"]');
+let savedMovieSearchTerm;
 
-let getMovieUrl = (imdbID) => {
-    let movieUrl = `https://www.imdb.com/title/${imdbID}/`;
-    return movieUrl;
-}
 
-console.log(`step 4: `)
+const getMovieUrl = imdbID => `https://www.imdb.com/title/${imdbID}/`;
+
 const addMoviesToDom = movies => {
     const movieList = document.querySelector("#movieList")
-    movieList.innerHTML = ""
-    const noMoviesToShow = document.querySelector(".no-movies-to-show")
-    noMoviesToShow.innerHTML = ""
+    movieList.innerHTML = "";
+    const noMoviesToShow = document.querySelector(".no-movies-to-show");
+    noMoviesToShow.innerHTML = "";
 
-    for (let movie of movies) {
-        const  movieListInHtml = `<li class="movieListInHtml"><a class="movie__html" href="${getMovieUrl(movie?.imdbID)}" target="_blank"><img class = "grid-container__movie__link" src = ${movie?.Poster} ></img></a></li>`
+    for (const movie of movies) {
+        const movieListInHtml = `<li class="movieListInHtml"><a class="movie__html" href="${getMovieUrl(movie?.imdbID)}" target="_blank"><img class = "grid-container__movie__link" src = ${movie?.Poster} ></img></a></li>`;
         movieList.insertAdjacentHTML('beforeend', movieListInHtml);
     }
     if (movies.length === 0) {
-        const movieListInHtml2 = `<div class="no-movies-to-show">"Deze zoekterm levert geen resultaten op in de film database. Probeert u een andere zoekterm."</div>`
+        const movieListInHtml2 = `<div class="no-movies-to-show">"Deze zoekterm levert geen resultaten op in de film database. Probeert u een andere zoekterm."</div>`;
         noMoviesToShow.insertAdjacentHTML('beforeend', movieListInHtml2);
     }
 }
 
-const filterMovies = (wordInMovieTitle) => {
-    let filterMovieTitle = movies       
-        .filter(movie =>     
-            movie.Title.toLowerCase().includes(wordInMovieTitle?.toLowerCase()))
+addMoviesToDom(movies);
+
+const filterMovies = wordInMovieTitle => {
+    let filterMovieTitle = movies
+        .filter(movie =>
+            movie.Title.toLowerCase().includes(wordInMovieTitle?.toLowerCase()));
     filterMovieTitle = filterMovieTitle ?? [];
-    return filterMovieTitle;
+    addMoviesToDom(filterMovieTitle);
 }
 
-const filterOneMovie = (imdbID) => {
-    let filterMovieTitle = movies       
-        .filter(movie => movie.imdbID === imdbID)
-    return filterMovieTitle;
+const filterLatestMovies  = () => {
+    const latestMovies = movies
+        .filter(movie => parseInt(movie.Year) >= 2014);
+    addMoviesToDom(latestMovies);
 }
 
-let savedMovieSearchTerm;
+const filterOneMovie = imdbID => {
+    const filterMovieTitle = movies
+        .filter(movie => movie.imdbID === imdbID);
+    addMoviesToDom(filterMovieTitle);
+}
+
+const getRandomIntFromInterval = () => Math.floor(Math.random() * movies.length);
+
+const getMovieImdbIdRandom = () => {
+    const randomNumber = getRandomIntFromInterval();
+    let randomMovieImdbId = movies[randomNumber]?.imdbID;
+    return randomMovieImdbId;
+}
 
 
-const inputFieldSearchTerm = document.querySelector('.flex-container__inputfield-search-term');
-
-inputFieldSearchTerm.addEventListener('change', function () {
+inputFieldSearchTerm.addEventListener('change', function (event) {
     // preserve  search-term-value (e.g. "de") from input field, so this search-term can be re-used after e.g. clicking on button 'Toon willekeurige film' or clicking on  a radiobutton.
-    savedMovieSearchTerm = this.value;
-    this.value = this.value ?? savedMovieSearchTerm;
-    addMoviesToDom(filterMovies(savedMovieSearchTerm))
+    savedMovieSearchTerm = event.target.value;
+    event.target.value = event.target.value ?? savedMovieSearchTerm;
+    return filterMovies(savedMovieSearchTerm);
 });
 
-
-const inputSearchButton = document.querySelector('.flex-container__search-button');
-inputSearchButton.addEventListener('click', function () {
-    this.value = this.value ?? savedMovieSearchTerm;
-    addMoviesToDom(filterMovies(savedMovieSearchTerm))
+inputSearchButton.addEventListener('click', function (event) {
+    event.target.value = event.target.value ?? savedMovieSearchTerm;
+    return filterMovies(savedMovieSearchTerm);
 });
 
 document.addEventListener('keydown', function (event) {
     if (inputFieldSearchTerm === document.activeElement) {
         if (event.key === "Enter") {
-            addMoviesToDom(filterMovies(savedMovieSearchTerm))
+            filterMovies(savedMovieSearchTerm);
         }   
     }
 })
 
-let buttonSearchRandom = document.querySelector('.flex-container__random-search-button');
-buttonSearchRandom.addEventListener('click', function(){ addMoviesToDom(filterOneMovie(getMovieImdbIdRandom()))});
-
-
-const filterLatestMovies  = () => {
-let latestMovies = movies
-    .filter(movie => parseInt(movie.Year) >= 2014)
-    console.log(filterLatestMovies) 
-    console.log(latestMovies) 
-return latestMovies;
-}
-
-addMoviesToDom(filterLatestMovies())
+buttonSearchRandom.addEventListener('click', function(){ filterOneMovie(getMovieImdbIdRandom())});
 
 const handleOnChangeEvent = event => {
-    let radioButtonSelected = event.target.value;
-    let selectedMovies;
-    console.log(event.target.value)
-
+    const radioButtonSelected = event.target.value;
     switch (radioButtonSelected) {
+        case 'allFilms':
+            addMoviesToDom(movies);
+            break;        
         case 'latestFilms':
-            selectedMovies = filterLatestMovies()
+            filterLatestMovies();
             break;
         case 'avengerFilms':
-            selectedMovies = filterMovies("Avenger")
+            filterMovies("Avenger");
             break;
         case 'x-menFilms':
-            selectedMovies = filterMovies("X-Men")
+            filterMovies("X-Men");
             break;
         case 'princessFilms':
-            selectedMovies = filterMovies("Princess")
+            filterMovies("Princess");
             break;
         case 'batmanFilms':
-            selectedMovies = filterMovies("Batman")
+            filterMovies("Batman");
             break;
         default:
             console.log(` The selected radiobutton < ${radioButtonSelected} > is not a valid choice.`);
-}
-    addMoviesToDom(selectedMovies)
+    }
 }
 
-let radios = document.querySelectorAll('input[type=radio][name="film-filter"]');
 radios.forEach(radio => radio.addEventListener('change', handleOnChangeEvent));
 
-const getRandomIntFromInterval = () => Math.floor(Math.random() * movies.length);
 
-let getMovieImdbIdRandom = () => {
-    const randomNumber = getRandomIntFromInterval()
-    let randomMovieImdbId = movies[randomNumber]?.imdbID
-    return randomMovieImdbId;
-}
+
  
